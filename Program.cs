f10025b6cb605ec6403905b98bec0b2c68476074
JoshGoodman22 <jgoodman2024@chadwickschool.org>
2022-01-13 11:47:41 -0800

    Commits to add to PR

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jgoodman_BinaryNumbersInCSha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
